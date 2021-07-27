9c04458152ca0808177f200025ff2ecd5787d35e
Maria Gileto <gileto@bk.ru>
2021-07-28 00:03:38 +0300

    feat: added classes for likes

--- src/components/item.vue
+++ src/components/item.vue
@@ -1,11 +1,11 @@
 <template>
-    <li>
+    <li v-bind:class="{done: anec.liked}">
     {{anec.text}}
-    <button>Like</button>
+    <button v-bind:class="{doneBtn: anec.liked}"> Like </button>
     </li>
 </template>
 
-<<script>
+<script>
 export default {
     props: {
         anec: {
@@ -31,4 +31,18 @@ export default {
         margin-left: auto;
         color: gray;
     }
+
+    .done {
+        background: Honeydew;
+        font-weight: bold;
+        border-color: DarkGray;
+    }
+
+    .doneBtn {
+        color: Black;
+        background: LavenderBlush;
+        border-color: Orchid;
+
+    }
+
 </style>
